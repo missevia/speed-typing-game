638e7376cb6b1910d03a74a92e299c3c4dad3406
missevia <evija.horoneko@gmail.com>
2020-01-15 14:29:58 +0000

    added a countdown with useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import './App.css'
 
 function App() {
 
   const [text, setText] = useState("")
+  const [timeRemaining, setTimeRemaining] = useState(5)
+  const [gameStarted, setGameStarted] = useState(false)
     
   function handleChange(event) {
       const {value} = event.target
@@ -13,9 +15,18 @@ function App() {
   function calculateWords() {
     // trim removes all whitespace
     const words = text.trim().split(" ")
-    console.log(words.length)
-    return words.length
+    return words.filter(word => word !== "").length
   }
+
+  useEffect(() => {
+    if (timeRemaining > 0 && gameStarted === true) {
+      setTimeout(() => {
+        setTimeRemaining(prevTime => prevTime - 1)
+      }, 1000)
+    } else {
+      setGameStarted(false)
+    }      
+  }, [timeRemaining, gameStarted])
 
   return (
       <div>
@@ -24,8 +35,8 @@ function App() {
             onChange={handleChange} 
             value={text}
           />
-          <h2>Time remaining: 5</h2>
-          <button onClick={calculateWords}>start game</button>
+          <h2>Time remaining: {timeRemaining}</h2>
+          <button onClick={() => {setGameStarted(true)}}>start game</button>
           <h1>Word count: 0</h1>
       </div>
   )
